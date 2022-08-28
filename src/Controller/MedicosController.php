8d87849dab297d2dad79f464f2a2f3009b3bac22
João Pedro <joao.pedreira@estudante.ufjf.br>
2022-08-28 01:41:34 -0300

    Adiciona entityManager para salvar dados no sqlite

--- src/Controller/MedicosController.php
+++ src/Controller/MedicosController.php
@@ -2,14 +2,24 @@
 
 namespace App\Controller;
 
-use App\Entity\Medico;
+use  App\Entity\Medico;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class MedicosController
 {
+    /**
+     * @var EntityManagerInterface
+     */
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
 
     /**
      * 
@@ -19,10 +29,12 @@ class MedicosController
     {
         $corpoDaRequesicao = $request->getContent();
         $dadoEmJson = json_decode($corpoDaRequesicao);
-        
         $medico = new Medico();
         $medico->crm = $dadoEmJson->crm;
         $medico->nome = $dadoEmJson->nome;
+
+        $this->entityManager->persist($medico);
+        $this->entityManager->flush();
 
         return new JsonResponse($medico);
     }
--- src/Entity/Medico.php
+++ src/Entity/Medico.php
@@ -1,11 +1,14 @@
 <?php
 
 namespace App\Entity;
+use Doctrine\ORM\Mapping as ORM;
 
+/**
+ * @ORM\Entity()
+ */
 class Medico 
 {
     public int $crm;
     public string $nome;
 
-    
 }
