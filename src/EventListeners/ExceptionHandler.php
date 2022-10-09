f7dc2f0ea95e1a4442a641472410cb9b6ed310da
João Pedro <joao.pedreira@estudante.ufjf.br>
2022-10-09 19:38:29 -0300

    Adiciona event subscriber para retorno de erro mais amigavel para rota invalida

---
+++ src/EventListeners/ExceptionHandler.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\EventListeners;
+
+use App\Helper\ResponseFactory;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class ExceptionHandler implements EventSubscriberInterface
+{
+
+    public static function getSubscribedEvents()
+    {
+        // TODO: Implement getSubscribedEvents() method.
+        return [
+            KernelEvents::EXCEPTION => 'handle404Exception',
+        ];
+    }
+
+    public function handle404Exception(ExceptionEvent $event)
+    {
+        if($event->getThrowable() instanceof NotFoundHttpException) {
+            $event->setResponse(new JsonResponse([
+                'Mensagem' => new ResponseFactory(),
+            ]));
+            /*
+            //Caso queira redirecionar para outra rota caso a rota digitado pelo usuario não existe
+            $event->setResponse(new RedirectResponse('http://localhost:8080/medicos'));
+            */
+        }
+    }
+}
